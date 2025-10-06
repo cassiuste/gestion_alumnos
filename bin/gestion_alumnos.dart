e78704d6cb9960ee9887ae2c98d8d3dfea31e01e
cassius <cassiustedesco1@gmail.com>
2025-10-06 18:24:09 +0200

    Implemented sortModules and findModule

--- bin/gestion_alumnos.dart
+++ bin/gestion_alumnos.dart
@@ -2,17 +2,38 @@ import 'module.dart';
 import 'student.dart';
 import 'globals.dart';
 void main(List<String> arguments) {
-  sortModules();
+
+  List<Module> studentModules = [];
+  studentModules.add(modules["M02"]!);
+  studentModules.add(modules["M01"]!);
+  studentModules.add(modules["M05"]!);
+  studentModules.add(modules["M03"]!);
+  Student juan = new Student(name: "Juan", lastName: "Diaz", dni: 111111, age: 19, modules: studentModules, repeater: false);
+  sortModules(juan);
 }
 
 void sortModules(Student student){
-  print();
+  List<Module> modules =  student.modules;
+  print("Lista original: $modules");
+  modules.sort((a, b) => a.name.compareTo(b.name));
+  print("Lista Ordenada: $modules");
 }
 
 bool findModule(Student student, String moduleName){
-  return true;
+  for (Module module in student.modules) {
+    String name = module.name;
+    if (name.compareTo(moduleName) == 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
-void validateNumberOfModules(){
-
+void validateNumberOfModules(Student student){
+  if (student.modules.length >= 4) {
+    print("Curso completo");
+  }
+  else{
+    
+  }
 }
